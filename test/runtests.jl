496f0af4482068de077c9ab64629eb450cfed81f
Lukas Weber <49278367+lukas-weber@users.noreply.github.com>
2023-04-07 17:23:39 -0400

    add tests

---
+++ test/runtests.jl
@@ -0,0 +1,4 @@
+using Test
+import StochasticSeriesExpansion as S
+
+include("test_opercode.jl")
---
+++ test/test_opercode.jl
@@ -0,0 +1,24 @@
+@testset "OperCode" begin
+    for bond in rand(1:100*100, 20)
+        for vidx in rand(1:77, 20)
+            for diagonal in [true, false] 
+                v = S.VertexCode(diagonal, vidx)
+
+                @test !S.isinvalid(v)
+
+                @test S.isdiagonal(v) == diagonal
+                @test S.get_vertex_idx(v) == vidx
+
+                op = S.OperCode(bond, v)
+
+                @test S.get_bond(op) == bond
+                @test S.isdiagonal(op) == diagonal
+                @test S.get_vertex(op).code == v.code
+
+                @test !S.isidentity(op)
+            end
+        end
+    end
+
+    @test S.isidentity(S.OperCode(S.Identity))
+end
