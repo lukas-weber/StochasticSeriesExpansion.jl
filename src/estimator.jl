94321f6fad7b874bc83c101d124f5aaa5d30c28a
Lukas Weber <lweber@mamecha>
2023-04-11 21:06:43 -0400

    estimator: stub for opstring estimators

---
+++ src/estimator.jl
@@ -0,0 +1,50 @@
+abstract type AbstractEstimator end
+
+macro stub(func::Expr)
+    return :(
+        $func = error(
+            "AbstractEstimator interface not implemented for Estimator of type $(typeof(est))",
+        )
+    )
+end
+
+@stub init(est::AbstractEstimator, state::AbstractVector{<:StateIdx})
+
+@stub measure(est::AbstractEstimator, op::OperCode, state::AbstractVector{<:StateIdx}, sse_data::SSEData)
+
+@stub result(est::AbstractEstimator, ctx::MCContext)
+
+function opstring_measurement(mc::MC{Model}, ctx::MCContext, estimators::AbstractEstimator...) where {Model}
+    for estimator in estimators
+        init!(estimator, mc.state)
+    end
+
+    n = zero(Int64)
+
+    for op in mc.operators
+        if isidentity(op)
+            continue
+        end
+
+        if !isdiagonal(op)
+            b = mc.sse_data.bonds[get_bond(op)]
+            vd = get_vertex_data(get_bond(op))
+
+            leg_state = @view vd.leg_states[:,get_vertex(op)]
+
+            mc.state[b.sites...] .= leg_state[leg_count(Model)÷2:end]
+        end
+
+        if(n < mc.num_operators)
+            for estimator in estimators
+                measure(estimator, op, mc.state, mc.sse_data)
+            end
+        end
+
+        n += 1
+    end
+
+    for estimator in estimators
+        result(estimator, ctx)
+    end
+end
