76dd81084beadaccfb83716da81eb2f9a900f685
Lukas Weber <lweber@mamecha>
2023-04-16 20:55:46 -0400

    test: separate out ed_test

---
+++ test/test_ed_compare.jl
@@ -0,0 +1,39 @@
+using LoadLeveller.ResultTools
+using Distributions
+using HypothesisTests
+using Measurements
+
+include("ed/ed.jl")
+include("test_jobs.jl")
+
+
+function run_mc(job::JobInfo)
+    LoadLeveller.start(LoadLeveller.SingleRunner{job.mc}, job)
+
+    return DataFrame(ResultTools.dataframe("$(job.dir)/../$(job.name).results.json"))
+end
+
+@testset "ED Comparison" begin
+    mktempdir() do tmpdir
+        jobs = generate_test_jobs(tmpdir, 100000, 10000)
+
+        for (jobname, job) in jobs
+            @testset "$(job.name)" begin
+                get_model(::Type{S.MC{Model,NSites}}) where {Model,NSites} = Model
+                model = get_model(job.mc)
+                (obsnames, ed_data) = run_ed(model, job)
+                mc_data = run_mc(job)
+
+                for obsname in obsnames
+                    z = stdscore.(mc_data[!, obsname], ed_data[!, obsname])
+                    p = pvalue(ExactOneSampleKSTest(z, Normal()))
+                    if p <= 1e-3
+                        println("$obsname: p = $p")
+                    end
+
+                    @test p > 1e-3
+                end
+            end
+        end
+    end
+end
--- test/test_jobs.jl
+++ test/test_jobs.jl
@@ -1,27 +1,23 @@
 using LoadLeveller
 using LoadLeveller.JobTools
-using LoadLeveller.ResultTools
-using Distributions
-using HypothesisTests
-using Measurements
-
-include("ed/ed.jl")
 
 function generate_test_jobs(
     jobdir::AbstractString,
     sweeps::Integer,
     thermalization::Integer,
 )
-    jobs = JobInfo[]
+    jobs = Dict{String,JobInfo}()
 
-    function test_job(name::AbstractString, ::Type{Model}, tm::TaskMaker) where {Model}
-        return JobInfo(
+    function add_test_job(name::AbstractString, ::Type{Model}, tm::TaskMaker) where {Model}
+        jobs[name] = JobInfo(
             "$jobdir/$name",
             S.mc(Model);
             run_time = "15:00",
             checkpoint_time = "15:00",
             tasks = make_tasks(tm),
         )
+
+        return nothing
     end
 
     tm = TaskMaker()
@@ -42,38 +38,7 @@ function generate_test_jobs(
         task(tm; T = T)
     end
 
-    push!(jobs, test_job("magnet_square", S.Models.Magnet, tm))
+    add_test_job("magnet_square", S.Models.Magnet, tm)
 
     return jobs
-end
-
-function run_mc(job::JobInfo)
-    LoadLeveller.start(LoadLeveller.SingleRunner{job.mc}, job)
-
-    return DataFrame(ResultTools.dataframe("$(job.dir)/../$(job.name).results.json"))
-end
-
-@testset "ED Comparison" begin
-    mktempdir() do tmpdir
-        jobs = generate_test_jobs(tmpdir, 100000, 10000)
-
-        for job in jobs
-            @testset "$(job.name)" begin
-                get_model(::Type{S.MC{Model,NSites}}) where {Model,NSites} = Model
-                model = get_model(job.mc)
-                (obsnames, ed_data) = run_ed(model, job)
-                mc_data = run_mc(job)
-
-                for obsname in obsnames
-                    z = stdscore.(mc_data[!, obsname], ed_data[!, obsname])
-                    p = pvalue(ExactOneSampleKSTest(z, Normal()))
-                    if p <= 1e-3
-                        println("$obsname: p = $p")
-                    end
-
-                    @test p > 1e-3
-                end
-            end
-        end
-    end
 end
